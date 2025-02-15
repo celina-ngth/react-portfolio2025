2ecce48f0575442c3c5c7cf2a7fa930b37a4732b
celina-ngth <ngeth.celina@gmail.com>
2025-02-15 16:13:09 +0100

    feat: update components

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,13 +1,11 @@
 const Tool = ({ tool }) => {
   return (
     <>
-      <div className="flex flex-wrap gap-3">
-        {tool && (
-          <div className="bg-secondary flex flex-col items-center rounded-lg px-3 py-2 font-semibold">
-            {tool}
-          </div>
-        )}
-      </div>
+      {tool && (
+        <div className="bg-secondary flex flex-col items-center rounded-lg px-3 py-2 font-semibold">
+          {tool}
+        </div>
+      )}
     </>
   )
 }
