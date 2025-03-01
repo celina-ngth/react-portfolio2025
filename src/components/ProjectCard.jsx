f5c84424e882f542919e1b531fd0844ee420157c
celina-ngth <ngeth.celina@gmail.com>
2025-03-01 14:37:47 +0100

    feat: update import image method

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,15 +2,21 @@ import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 const ProjectCard = ({ project, small }) => {
-  const [imageSrc, setImageSrc] = useState(null)
+  const [imageSrcs, setImageSrcs] = useState({})
+
   useEffect(() => {
-    if (project.images?.[0]) {
-      import(
-        /* @vite-ignore */ `../assets/projects/${project.images[0]}.webp`
-      ).then((image) => {
-        setImageSrc(image.default)
-      })
+    const loadImageSrcs = async () => {
+      if (project.images) {
+        const imageSrcs = await Promise.all(
+          project.images?.map(async (imageName) => {
+            const image = await import(`../assets/projects/${imageName}.webp`)
+            return image.default
+          }),
+        )
+        setImageSrcs((prevImageSrcs) => ({ ...prevImageSrcs, ...imageSrcs }))
+      }
     }
+    loadImageSrcs()
   }, [project.images])
 
   return (
@@ -21,13 +27,13 @@ const ProjectCard = ({ project, small }) => {
           className={`${small || project.size === 'small' ? 'md:col-span-1 md:row-span-1' : 'md:col-span-2 md:row-span-2'} `}
         >
           <div className="bg-primary group flex h-full flex-col rounded-xl">
-            {imageSrc && (
+            {imageSrcs && (
               <div
                 className={`bg-neutral-light flex-grow overflow-hidden rounded-t-xl ${small || project.size === 'small' ? 'max-h-[200px]' : ''}`}
               >
                 <img
                   className="h-full w-full scale-101 rounded-t-xl object-cover object-top opacity-90 duration-200 ease-in-out hover:translate-0 hover:scale-102 hover:opacity-99"
-                  src={imageSrc}
+                  src={imageSrcs[0]}
                   alt=""
                 />
               </div>
